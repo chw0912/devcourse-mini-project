587977125c70b49231608b497ceb64d5e1246902
chw0912 <gmldnd28@gmail.com>
2025-03-21 07:51:00 +0900

    1-2 요구사항 구현 완료

--- src/main/java/controller/CommandController.java
+++ src/main/java/controller/CommandController.java
@@ -4,14 +4,26 @@ import view.InputView;
 import view.OutputView;
 
 public class CommandController {
+
+    private boolean programStatus = true;
     public CommandController() {
 
     }
 
     public void inputCommand() {
-        while (true) {
+        while (programStatus) {
             OutputView.printCommandString();
-            OutputView.printCommand(InputView.commandInput());
+            String command = InputView.commandInput();
+
+            switch (command) {
+
+                case OutputView.EXIT_COMMAND -> {
+                    OutputView.printExitCommand();
+                    programStatus = false;
+                }
+                default -> OutputView.printNotExistsCommand();
+            }
+
         }
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,14 +3,21 @@ package view;
 public class OutputView {
 
     private static final String COMMAND = "명령어 > ";
+    public static final String EXIT_COMMAND = "exit";
+
 
     public static void printCommandString() {
         System.out.print(COMMAND);
     }
 
-    public static void printCommand(String command) {
-        System.out.println(command);
+    public static void printExitCommand() {
+        System.out.println("프로그램을 종료합니다.");
     }
+
+    public static void printNotExistsCommand() {
+        System.out.println("존재하지 않는 명령어입니다.");
+    }
+
 
 }
 
